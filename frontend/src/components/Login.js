35ec25f418987af46ed09f3bc5d29bc892467b03
Nguyen Thanh <nvthanh12102004@gmail.com>
2024-10-15 15:21:02 +0700

    update Controller

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,15 +2,16 @@ import React, { useRef, useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
-    const { user, setUser } = useState({});
+    const [user, setUser] = useState({});
     const username = useRef("");
     const password = useRef("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await axios.post('http://localhost:5000/auth/login', { username: username.current.value, password: password.current.value }, { withCredentials: true })
-            .then((res) => setUser(res.data))
-            .catch((err) => console.log(err));
+        const res = await axios.post('http://localhost:5000/auth/login', { username: username.current.value, password: password.current.value }, { withCredentials: true })
+        console.log(JSON.stringify(res.data))
+        setUser(res.data);
+        console.log(JSON.stringify(user));
     }
 
     return (
@@ -21,12 +22,9 @@ const Login = () => {
                 <input type="text" id="username" ref={username} />
                 <br />
                 <label for="password" >Password</label>
-                <input type="password" id="password" ref={password} />
+                <input type="text" id="password" ref={password} />
                 <button type="submit">Login</button>
             </form>
-            <div>
-                {JSON.stringify(user)}
-            </div>
         </div>
     );
 }
