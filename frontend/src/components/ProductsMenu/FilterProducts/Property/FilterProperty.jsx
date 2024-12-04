86ea3185043290bd6b7ae0781733a5efcf5cf579
thanh2k4 <nvthanh12102004@gmail.com>
2024-12-04 17:15:56 +0700

    p

--- frontend/src/components/ProductsMenu/FilterProducts/Property/FilterProperty.jsx
+++
@@ -1,9 +0,0 @@
-export const FilterProperty = (products, property) => {
-    if (!products || !Array.isArray(products)) return [];
-    if (!property || property === "all") return products;
-
-    return products.filter(product => product.property === property);
-};
-
-
-
