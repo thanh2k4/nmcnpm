f7c59f1d73f19336d3ddc0f1845a367338f2fc29
thanh2k4 <nvthanh12102004@gmail.com>
2024-12-18 21:15:18 +0700

    remove carttoorder

--- backend/src/controllers/CartController.js
+++ backend/src/controllers/CartController.js
@@ -50,54 +50,10 @@ const updateCart = async (req, res) => {
     }
 }
 
-// Create orders from products selected in cart , products selected is parameter
-const cartToOrder = async (req, res) => {
-    const userId = req.user.userId;
-    const { products } = req.body;
-    try {
-        const cart = await Cart.findOne({
-            where: {
-                userId: userId
-            },
-            include: Product,
-            attributes: ['quantity', "price"]
-        });
-
-        const order = await Order.create({
-            userId: userId,
-            status: 'pending',
-            totalPrice: 0,
-        });
-
-        let totalPrice = 0;
-
-        for (const selectedProduct of products) {
-            const cartProduct = cart.Products.find(product => product.productId === selectedProduct.productId);
-            if (cartProduct) {
-                totalPrice += cartProduct.price * selectedProduct.quantity;
-                await OrderProduct.create({
-                    orderId: order.orderId,
-                    productId: selectedProduct.productId,
-                    quantity: selectedProduct.quantity,
-                    price: cartProduct.price
-                });
-
-                await cart.removeProduct(selectedProduct.productId);
-
-            }
-        }
-        order.totalPrice = totalPrice;
-        await order.save();
-        return res.status(201).json(order);
-    } catch (error) {
-        return res.status(500).send(error);
-    }
-}
 
 
 
 module.exports = {
     getCart,
-    updateCart,
-    cartToOrder,
+    updateCart
 }
--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCart, updateCart, cartToOrder } = require('../controllers/CartController');
+const { getCart, updateCart } = require('../controllers/CartController');
 const { verifyAccessToken } = require('../middlewares/verifyAccessToken');
 const { authorizeRoles } = require('../middlewares/authorizeRoles');
 
@@ -8,6 +8,5 @@ router.get('/', verifyAccessToken, getCart);
 
 router.patch('/', verifyAccessToken, updateCart);
 
-router.post('/', verifyAccessToken, cartToOrder);
 
 module.exports = router;
