1d75b8b407ab13607a5595d81f745f1630399d98
thanh2k4 <nvthanh12102004@gmail.com>
2024-12-23 21:47:12 +0700

    fix

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -10,11 +10,11 @@ router.post('/', verifyAccessToken, authorizeRoles("USER", "ADMIN", "STAFF"), cr
 // Route: Get all orders (Admin and Staff only)
 router.get('/', verifyAccessToken, authorizeRoles("ADMIN", "STAFF"), getOrders);
 
-// Route: Get all orders by user id (Admin and Staff only)
-router.get('/user/:userId', verifyAccessToken, authorizeRoles("ADMIN", "STAFF"), getOrdersByUserId);
-
 // Route: Get orders by current user (only the user can access their own orders)
 router.get('/user', verifyAccessToken, getOrdersByUser);
+
+// Route: Get all orders by user id (Admin and Staff only)
+router.get('/user/:userId', verifyAccessToken, authorizeRoles("ADMIN", "STAFF"), getOrdersByUserId);
 
 // Route: Get an order by id (Admin, Staff, or the User)
 router.get('/:id', verifyAccessToken, authorizeRoles("ADMIN", "STAFF", "USER"), getOrderById);
@@ -26,4 +26,4 @@ router.delete('/:id', verifyAccessToken, authorizeRoles("ADMIN"), deleteOrder);
 router.patch('/:id', verifyAccessToken, authorizeRoles("ADMIN", "STAFF"), updateOrder);
 
 module.exports = router;
-        
+
